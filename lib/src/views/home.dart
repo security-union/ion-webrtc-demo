78bfadb617b4a26e30b1cb2a0d6b58ab92834b7b
Dario A Lencina-Talarico <darioalessandrolencina@gmail.com>
2021-07-19 00:30:32 -0400

    [RSH-46] Client task: generate uuid and store it locally  (#4)

--- lib/src/views/home.dart
+++ lib/src/views/home.dart
@@ -1,11 +1,9 @@
 // ignore_for_file: avoid_print
 import 'package:flutter/material.dart';
-import 'package:flutter_ion/flutter_ion.dart' as ion;
 import 'package:ion_webrtc_demo/src/styles/colors.dart';
 import 'package:ion_webrtc_demo/src/styles/text.dart';
 import 'package:ion_webrtc_demo/src/views/role_view.dart';
 import 'package:ion_webrtc_demo/src/widgets/rounded_button.dart';
-import 'package:uuid/uuid.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key, required this.title}) : super(key: key);
@@ -17,8 +15,6 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  // Our device Unique identifier
-  final String _uuid = const Uuid().v4();
   final _formKey = GlobalKey<FormState>();
   String _addr = "ws://192.168.18.11:7000/ws"; //"ws://192.168.18.11:7000/ws";
 
@@ -35,7 +31,7 @@ class _HomeState extends State<Home> {
             _formKey,
             onUpdateAddr: (String addr) => setState(() => _addr = addr),
             onValidSubmit: () {
-              _navigateToRoleView(_uuid, _addr);
+              _navigateToRoleView(_addr);
             },
           ),
         ),
@@ -95,12 +91,12 @@ class _HomeState extends State<Home> {
         },
       );
 
-  void _navigateToRoleView(String uuid, String addr) {
+  void _navigateToRoleView(String addr) {
     Navigator.push(
       context,
       MaterialPageRoute(
         builder: (BuildContext context) {
-          return RoleView(uuid: uuid, addr: addr);
+          return RoleView(addr: addr);
         },
       ),
     );
